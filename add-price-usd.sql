747ff082505d81acf9e90133205ed387829631f6
Khoa Nguyen <khoanv3@verichains.io>
2026-06-15 20:33:37 +0700

    chore: remove obsolete local scripts and database migration files

--- add-price-usd.sql
+++
@@ -1,2 +0,0 @@
--- Migration: thêm cột price_usd vào bảng properties
-ALTER TABLE properties ADD COLUMN IF NOT EXISTS price_usd BIGINT;
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-SET PATH=C:\Program Files\nodejs;%PATH%
-npm run dev
